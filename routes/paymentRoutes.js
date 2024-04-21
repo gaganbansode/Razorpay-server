3ebdeb85f1c4dc5b6346578a0bd5f8aca79b0757
gagan bansode <gaganbansode@gmail.com>
2024-04-21 15:46:41 +0530

    client upload

---
+++ routes/paymentRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {
+  checkout,
+  paymentVerification,
+} from "../controllers/paymentController.js";
+
+const router = express.Router();
+
+router.post("/checkout", checkout);
+router.post("/paymentVerification", paymentVerification);
+export default router;
